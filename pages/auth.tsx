18d2b6c525964d0c9a0bc32b614676cbb0b8f7e2
diogodittmanndev <diogo.dyaot@gmail.com>
2023-04-21 18:14:06 -0300

    first commit

---
+++ pages/auth.tsx
@@ -0,0 +1,54 @@
+import Input from "@/components/Input";
+import { useState } from "react";
+
+const Auth = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="Logo" className="h-12" />
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:max-w-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Entrar
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input
+                                label="Nome"
+                                onChange={(e: any) => setName(e.target.value)}
+                                id="name"
+                                type="name"
+                                value={name}
+                            />
+
+                            <Input
+                                label="Email"
+                                onChange={(e: any) => setEmail(e.target.value)}
+                                id="email"
+                                type="email"
+                                value={email}
+                            />
+
+                            <Input
+                                label="Senha"
+                                onChange={(e: any) =>
+                                    setPassword(e.target.value)
+                                }
+                                id="password"
+                                type="password"
+                                value={password}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Auth;
