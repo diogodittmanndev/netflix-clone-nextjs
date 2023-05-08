104d2f2be1577a1260104367ad4f0d0e51b3c986
diogodittmanndev <diogo.dyaot@gmail.com>
2023-05-07 21:20:36 -0300

    Implemented login and registration

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,12 @@
 import Input from "@/components/Input";
 import axios from "axios";
 import { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Auth = () => {
+    const router = useRouter();
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -15,6 +19,21 @@ const Auth = () => {
         );
     }, []);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: "/",
+            });
+
+            router.push("/");
+        } catch (err) {
+            console.log(err);
+        }
+    }, [email, password, router]);
+
     const register = useCallback(async () => {
         try {
             await axios.post("/api/register", {
@@ -22,10 +41,12 @@ const Auth = () => {
                 name,
                 password,
             });
+
+            login();
         } catch (err) {
             console.log(err);
         }
-    }, []);
+    }, [email, name, password, login]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -43,7 +64,7 @@ const Auth = () => {
                                 <Input
                                     label="Nome"
                                     onChange={(e: any) =>
-                                        setEmail(e.target.value)
+                                        setName(e.target.value)
                                     }
                                     id="name"
                                     type="name"
@@ -69,7 +90,10 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700">
+                        <button
+                            onClick={variant === "login" ? login : register}
+                            className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
+                        >
                             {variant === "login" ? "Entrar" : "Cadastrar"}
                         </button>
                         <p className="text-neutral-500 mt-12">
